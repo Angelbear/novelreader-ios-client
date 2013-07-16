58175eeeaf2c71f1f84e65dd61424fd794d85822
Yangyang Zhao <yangyang.zhao.thu@gmail.com>
2013-07-16 10:17:53 +0900

    -n fix scroll

--- xiaoshuoyuedu/xiaoshuoyuedu/class/ui/MSReaderViewController.m
+++ xiaoshuoyuedu/xiaoshuoyuedu/class/ui/MSReaderViewController.m
@@ -79,7 +79,7 @@ CGFloat _cellHeight;
     UISearchBar* searchBar = [[UISearchBar alloc] init];
     searchBar.frame=CGRectMake(0, self.tableView.frame.origin.y, self.tableView.frame.size.width, 44);
     [searchBar sizeToFit];
-    self.tableView.tableHeaderView = searchBar;
+    //self.tableView.tableHeaderView = searchBar;
     self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(self.tableView.frame.origin.x, self.tableView.frame.origin.y, self.tableView.frame.size.width, 44)];
     self.searchBar.placeholder = @"搜索";
     self.searchBar.delegate = self;
@@ -91,13 +91,35 @@ CGFloat _cellHeight;
     self.strongSearchDisplayController.searchResultsDelegate = self;
     
     
-    self.tableView.tableHeaderView = self.searchBar;
+    /*self.tableView.tableHeaderView = self.searchBar;
+    
     
     SEL setPinsTableHeaderViewSelector = NSSelectorFromString(@"_setPinsTableHeaderView:");
     if ([self.tableView respondsToSelector:setPinsTableHeaderViewSelector]) {
         objc_msgSend(self.tableView, setPinsTableHeaderViewSelector, YES);
-    }
+    }*/
+    
+    [self.tableView addSubview:self.searchBar];
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(CGRectGetHeight(self.searchBar.bounds), 0, 0, 0);
+    self.tableView.scrollIndicatorInsets = UIEdgeInsetsMake(CGRectGetHeight(self.searchBar.bounds), 0, 0, 0);
 
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    if (scrollView == self.tableView) { // Don't do anything if the search table view get's scrolled
+        if (scrollView.contentOffset.y < -CGRectGetHeight(self.searchBar.bounds)) {
+            self.searchBar.layer.zPosition = 0; // Make sure the search bar is below the section index titles control when scrolling up
+        } else {
+            self.searchBar.layer.zPosition = 1; // Make sure the search bar is above the section headers when scrolling down
+        }
+        
+        CGRect searchBarFrame = self.searchBar.frame;
+        searchBarFrame.origin.y = MAX(scrollView.contentOffset.y, -CGRectGetHeight(searchBarFrame));
+        
+        self.searchBar.frame = searchBarFrame;
+    }
 }
 
 - (void)scrollTableViewToSearchBarAnimated:(BOOL)animated
